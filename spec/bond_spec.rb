4708f8d318c4ab7191e607056a393cfa108d47f0
Sam Phippen <samphippen@googlemail.com>
2015-07-16 14:49:48 +0100

    Green

--- spec/bond_spec.rb
+++ spec/bond_spec.rb
@@ -8,5 +8,18 @@ RSpec.describe Bond do
     it "returns a Bond::Spy" do
       expect(bond(:arbitrary)).to be_a_kind_of(Bond::Spy)
     end
+
+    xit "responds to all method calls and records them" do
+      my_spy = bond(:spy)
+      my_spy.this_method_name
+      bond_did_you_receive?(my_spy, :this_method_name)
+    end
+
+    it "fails the test when it didn't receive a method call" do
+      expect {
+        my_spy = bond(:spy)
+        bond_did_you_receive?(my_spy, :this_method_name)
+      }.to raise_error
+    end
   end
 end
