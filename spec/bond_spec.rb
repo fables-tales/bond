be07f208bcaef2c8adbaf24eb64f241cb3306e44
Sam Phippen <samphippen@googlemail.com>
2015-07-16 14:39:47 +0100

    Red

--- spec/bond_spec.rb
+++ spec/bond_spec.rb
@@ -1,7 +1,11 @@
 require "spec_helper"
 
 RSpec.describe Bond do
-  describe "#bond" do
+  include Bond
 
+  describe "#bond" do
+    it "returns a Bond::Spy" do
+      expect(bond(:arbitrary)).to be_a_kind_of(Bond::Spy)
+    end
   end
 end
