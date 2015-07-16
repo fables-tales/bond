f516f38f2dbc7397319d27e3c3e21d8e24031281
Sam Phippen <samphippen@googlemail.com>
2015-07-16 15:57:59 +0100

    Red

--- lib/bond.rb
+++ lib/bond.rb
@@ -3,10 +3,30 @@ require "bond/spy"
 
 module Bond
   def bond(name)
-    Bond::Spy.new(name)
+    Bond::Spy.new(name, proxy_callback)
   end
 
   def bond_did_you_receive?(spy, method_name)
-    raise "Expected to receive :#{method_name}"
+    unless spy_received_method?(spy)
+      raise "Expected to receive :#{method_name}"
+    end
+  end
+
+  private
+
+  def proxy_callback
+    lambda { |the_spy|
+      Bond::Proxy.new.tap { |proxy|
+        registered_proxies[the_spy] = proxy
+      }
+    }
+  end
+
+  def spy_received_method?(spy)
+
+  end
+
+  def registered_proxies
+    @registered_proxies ||= {}
   end
 end
