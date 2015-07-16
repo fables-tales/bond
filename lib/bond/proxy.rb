9be163fa890769291a8efb8acdfb4e638d763e10
Sam Phippen <samphippen@googlemail.com>
2015-07-16 16:21:39 +0100

    Red

--- lib/bond/proxy.rb
+++ lib/bond/proxy.rb
@@ -1,11 +1,19 @@
 module Bond
   class Proxy
-    def record_message_received(method_name)
+    def initialize
+      @received_messages = []
+    end
 
+    def record_message_received(method_name)
+      received_messages << method_name
     end
 
     def message_received?(method_name)
       false
     end
+
+    protected
+
+    attr_reader :received_messages
   end
 end
