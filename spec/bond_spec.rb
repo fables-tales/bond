5c8be6f181a00dc5004a1bef16da232ffe27e5aa
Sam Phippen <samphippen@googlemail.com>
2015-07-16 14:52:11 +0100

    Red

--- spec/bond_spec.rb
+++ spec/bond_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Bond do
   include Bond
 
   describe "#bond" do
+    let(:method_name) { :method_name }
+
     it "returns a Bond::Spy" do
       expect(bond(:arbitrary)).to be_a_kind_of(Bond::Spy)
     end
@@ -18,8 +20,8 @@ RSpec.describe Bond do
     it "fails the test when it didn't receive a method call" do
       expect {
         my_spy = bond(:spy)
-        bond_did_you_receive?(my_spy, :this_method_name)
-      }.to raise_error
+        bond_did_you_receive?(my_spy, method_name)
+      }.to raise_error("Expected to receive :#{method_name}")
     end
   end
 end
