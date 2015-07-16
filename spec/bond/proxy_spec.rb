12658a7f32db0a0185247d0f7246e8d8318fcc07
Sam Phippen <samphippen@googlemail.com>
2015-07-16 16:19:33 +0100

    Red

--- spec/bond/proxy_spec.rb
+++ spec/bond/proxy_spec.rb
@@ -5,6 +5,8 @@ module Bond
   RSpec.describe Proxy do
     subject(:proxy) { Proxy.new }
 
+    let(:method_name) { double(:method_name) }
+
     describe "#message_received?" do
       it "returns false when no message has been received" do
         expect(proxy.message_received?(method_name)).to be false
