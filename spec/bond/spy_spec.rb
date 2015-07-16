a7ec8646ad3b6fb351a5356f9bd6b43bec0f7ce6
Sam Phippen <samphippen@googlemail.com>
2015-07-16 15:39:46 +0100

    Red

---
+++ spec/bond/spy_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+module Bond
+  RSpec.describe Spy do
+    subject(:spy) {
+      Spy.new(:arbitrary)
+    }
+
+    describe "responding to everything" do
+      let(:method_name) { :method_name }
+
+      describe "#respond_to?" do
+        it "returns true" do
+          expect(spy.respond_to?(method_name)).to be true
+        end
+      end
+    end
+  end
+end
